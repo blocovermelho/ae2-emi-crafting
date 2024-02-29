b6aaef2c7b3d10afdddcd3c8717c6efd305e987f
alikindsys <alice@blocovermelho.org>
2024-02-29 14:32:46 -0300

    Add In-World Transformation Recipes

--- src/main/java/org/blocovermelho/ae2emicrafting/client/Ae2EmiPlugin.java
+++ src/main/java/org/blocovermelho/ae2emicrafting/client/Ae2EmiPlugin.java
@@ -6,6 +6,7 @@ import appeng.core.definitions.AEParts;
 import appeng.menu.AEBaseMenu;
 import appeng.menu.me.items.CraftingTermMenu;
 import appeng.menu.me.items.PatternEncodingTermMenu;
+import appeng.recipes.transform.TransformRecipe;
 import dev.emi.emi.api.EmiPlugin;
 import dev.emi.emi.api.EmiRegistry;
 import dev.emi.emi.api.recipe.VanillaEmiRecipeCategories;
@@ -14,6 +15,8 @@ import dev.emi.emi.api.widget.Bounds;
 import org.blocovermelho.ae2emicrafting.client.handler.Ae2PatternTerminalDragHandler;
 import org.blocovermelho.ae2emicrafting.client.handler.Ae2PatternTerminalHandler;
 import org.blocovermelho.ae2emicrafting.client.handler.Ae2RecipeHandler;
+import org.blocovermelho.ae2emicrafting.client.recipes.Ae2Categories;
+import org.blocovermelho.ae2emicrafting.client.recipes.ItemTransformationRecipe;
 
 public class Ae2EmiPlugin implements EmiPlugin {
     @Override
@@ -32,5 +35,8 @@ public class Ae2EmiPlugin implements EmiPlugin {
                         .forEach(consumer);
             }
         }));
+
+        registry.addCategory(Ae2Categories.WORLD_INTERACTION);
+        Ae2Categories.addAll(registry, TransformRecipe.TYPE, ItemTransformationRecipe::new);
     }
 }
