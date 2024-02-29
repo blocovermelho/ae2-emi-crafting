328962a35abec8e2727491baff86d595c923f35d
alikindsys <alice@blocovermelho.org>
2024-02-29 18:08:08 -0300

    Handle fluid stacks myself

--- src/main/java/org/blocovermelho/ae2emicrafting/client/handler/Ae2PatternTerminalDragHandler.java
+++ src/main/java/org/blocovermelho/ae2emicrafting/client/handler/Ae2PatternTerminalDragHandler.java
@@ -1,6 +1,5 @@
 package org.blocovermelho.ae2emicrafting.client.handler;
 
-import appeng.api.stacks.AEFluidKey;
 import appeng.api.stacks.GenericStack;
 import appeng.client.gui.me.items.PatternEncodingTermScreen;
 import appeng.core.sync.network.NetworkHandler;
@@ -9,10 +8,8 @@ import appeng.helpers.InventoryAction;
 import appeng.menu.me.items.PatternEncodingTermMenu;
 import dev.emi.emi.api.EmiDragDropHandler;
 import dev.emi.emi.api.stack.EmiStack;
-import net.minecraft.fluid.Fluid;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import net.minecraft.screen.slot.Slot;
+import org.blocovermelho.ae2emicrafting.client.helper.mapper.EmiStackConvertible;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -36,14 +33,8 @@ public class Ae2PatternTerminalDragHandler<C extends PatternEncodingTermMenu, T 
         }), (t, slot, emiIngredient) -> {
             List<EmiStack> emiStacks = emiIngredient.getEmiStacks();
             for (EmiStack emiStack : emiStacks) {
-                if (emiStack.getKey() instanceof Fluid fl) {
-                    ItemStack wrappedFluid = GenericStack.wrapInItemStack(new GenericStack(AEFluidKey.of(fl, emiStack.getNbt()), 81000L));
-                    NetworkHandler.instance().sendToServer(new InventoryActionPacket(InventoryAction.SET_FILTER, slot.id, wrappedFluid));
-                } else if (emiStack.getKey() instanceof Item){
-                    ItemStack itemStack = emiStack.getItemStack();
-                    NetworkHandler.instance().sendToServer(new InventoryActionPacket(InventoryAction.SET_FILTER, slot.id, itemStack));
-                }
-
+                GenericStack aeStack = EmiStackConvertible.AEGenericStack.into(emiStack);
+                NetworkHandler.instance().sendToServer(new InventoryActionPacket(InventoryAction.SET_FILTER, slot.id, GenericStack.wrapInItemStack(aeStack)));
             }
         });
     }
