b59cad33c6571d664fe261d6ab23036f436726fe
alikindsys <alice@blocovermelho.org>
2024-02-29 04:21:25 -0300

    Add Ae2PatternTerminalDragHandler.java
    First step towards ghost item support

--- src/main/java/org/blocovermelho/ae2emicrafting/client/Ae2EmiPlugin.java
+++ src/main/java/org/blocovermelho/ae2emicrafting/client/Ae2EmiPlugin.java
@@ -1,6 +1,7 @@
 package org.blocovermelho.ae2emicrafting.client;
 
 import appeng.client.gui.AEBaseScreen;
+import appeng.client.gui.me.items.PatternEncodingTermScreen;
 import appeng.core.definitions.AEParts;
 import appeng.menu.AEBaseMenu;
 import appeng.menu.me.items.CraftingTermMenu;
@@ -10,6 +11,7 @@ import dev.emi.emi.api.EmiRegistry;
 import dev.emi.emi.api.recipe.VanillaEmiRecipeCategories;
 import dev.emi.emi.api.stack.EmiStack;
 import dev.emi.emi.api.widget.Bounds;
+import org.blocovermelho.ae2emicrafting.client.handler.Ae2PatternTerminalDragHandler;
 import org.blocovermelho.ae2emicrafting.client.handler.Ae2PatternTerminalHandler;
 import org.blocovermelho.ae2emicrafting.client.handler.Ae2RecipeHandler;
 
@@ -18,7 +20,7 @@ public class Ae2EmiPlugin implements EmiPlugin {
     public void register(EmiRegistry registry) {
         registry.addWorkstation(VanillaEmiRecipeCategories.CRAFTING, EmiStack.of(AEParts.CRAFTING_TERMINAL.stack()));
         registry.addRecipeHandler(CraftingTermMenu.TYPE, new Ae2RecipeHandler<>());
-
+        registry.addDragDropHandler(PatternEncodingTermScreen.class, new Ae2PatternTerminalDragHandler<>());
         registry.addRecipeHandler(PatternEncodingTermMenu.TYPE, new Ae2PatternTerminalHandler<>());
 
         registry.addGenericExclusionArea(((screen, consumer) -> {
