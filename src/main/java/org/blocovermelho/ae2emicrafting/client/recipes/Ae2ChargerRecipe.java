857d3cbc0d9fcae879b2fe2bca42b270787ac433
alikindsys <alice@blocovermelho.org>
2024-03-01 16:03:36 -0300

    Port Ae2ChargerRecipe

--- src/main/java/org/blocovermelho/ae2emicrafting/client/recipes/Ae2ChargerRecipe.java
+++ src/main/java/org/blocovermelho/ae2emicrafting/client/recipes/Ae2ChargerRecipe.java
@@ -1,9 +1,13 @@
 package org.blocovermelho.ae2emicrafting.client.recipes;
 
+import appeng.blockentity.misc.ChargerBlockEntity;
+import appeng.blockentity.misc.CrankBlockEntity;
 import appeng.core.definitions.AEBlocks;
 import appeng.core.localization.ItemModText;
 import appeng.recipes.handlers.ChargerRecipe;
+import dev.emi.emi.api.recipe.BasicEmiRecipe;
 import dev.emi.emi.api.recipe.EmiRecipeCategory;
+import dev.emi.emi.api.render.EmiTexture;
 import dev.emi.emi.api.stack.EmiIngredient;
 import dev.emi.emi.api.stack.EmiStack;
 import dev.emi.emi.api.widget.WidgetHolder;
@@ -12,38 +16,29 @@ import org.blocovermelho.ae2emicrafting.client.recipes.category.Ae2CategoryHolde
 
 import java.util.List;
 
-public class Ae2ChargerRecipe extends BaseAe2Recipe {
+public class Ae2ChargerRecipe extends BasicEmiRecipe {
+    private final EmiIngredient ingredient;
+    private final EmiStack result;
     public Ae2ChargerRecipe(ChargerRecipe source) {
-        this.id = source.getId();
-
-        this.input = source.getIngredients()
-                .stream()
-                .filter(x -> !x.isEmpty())
-                .map(EmiIngredient::of)
-                .toList();
-
-        this.output = List.of(EmiStack.of(source.getResultItem()));
-
-        this.width = 130;
-        this.height = 50;
+        super(Ae2CategoryHolder.CHARGER, source.getId(), 130,50);
+        this.ingredient = EmiIngredient.of(source.getIngredient());
+        this.result = EmiStack.of(source.getResultItem());
     }
-    @Override
-    public EmiRecipeCategory getCategory() {
-        return Ae2CategoryHolder.CHARGER;
-    }
-
     @Override
     public void addWidgets(WidgetHolder widgets) {
-        widgets.addSlot(input.get(0), 31, 8);
-        widgets.addSlot(output.get(0), 81, 8).recipeContext(this);
+        widgets.addSlot(ingredient, 30, 7);
+        widgets.addSlot(result, 80, 7);
+        widgets.addSlot(EmiStack.of(AEBlocks.CRANK), 2, 29)
+                .drawBack(false);
 
-        widgets.addSlot(EmiStack.of(AEBlocks.CRANK), 3, 30).drawBack(false);
+        widgets.addTexture(EmiTexture.EMPTY_ARROW, 52, 8);
 
-        widgets.addFillingArrow(52, 8, 1);
-
-        int turns = 10;
-        MutableText text = ItemModText.CHARGER_REQUIRED_POWER.text(turns, 1600);
-
-        widgets.addText(text, 20, 35, 0x7E7E7E, false);
+        var turns = (ChargerBlockEntity.POWER_MAXIMUM_AMOUNT + CrankBlockEntity.POWER_PER_CRANK_TURN - 1)
+                / CrankBlockEntity.POWER_PER_CRANK_TURN;
+        widgets.addText(
+                ItemModText.CHARGER_REQUIRED_POWER.text(turns, ChargerBlockEntity.POWER_MAXIMUM_AMOUNT),
+                20, 35,
+                0x7E7E7E,
+                false);
     }
 }
