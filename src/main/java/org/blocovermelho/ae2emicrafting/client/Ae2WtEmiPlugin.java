31dbf258d300d25024169e22dbc481b03b2f1a81
alikindsys <alice@blocovermelho.org>
2024-03-01 22:58:14 -0300

    Update ae2wtlib terminals to new handlers

--- src/main/java/org/blocovermelho/ae2emicrafting/client/Ae2WtEmiPlugin.java
+++ src/main/java/org/blocovermelho/ae2emicrafting/client/Ae2WtEmiPlugin.java
@@ -1,17 +1,20 @@
 package org.blocovermelho.ae2emicrafting.client;
 
 import de.mari_023.ae2wtlib.wct.WCTMenu;
+import de.mari_023.ae2wtlib.wet.WETMenu;
 import de.mari_023.ae2wtlib.wut.WUTHandler;
 import dev.emi.emi.api.EmiPlugin;
 import dev.emi.emi.api.EmiRegistry;
 import dev.emi.emi.api.recipe.VanillaEmiRecipeCategories;
 import dev.emi.emi.api.stack.EmiStack;
-import org.blocovermelho.ae2emicrafting.client.handler.Ae2RecipeHandler;
+import org.blocovermelho.ae2emicrafting.client.handler.Ae2CraftingHandler;
+import org.blocovermelho.ae2emicrafting.client.handler.Ae2PatternTerminalHandler;
 
 public class Ae2WtEmiPlugin implements EmiPlugin {
     @Override
     public void register(EmiRegistry registry) {
         registry.addWorkstation(VanillaEmiRecipeCategories.CRAFTING, EmiStack.of(WUTHandler.wirelessTerminals.get("crafting").universalTerminal()));
-        registry.addRecipeHandler(WCTMenu.TYPE, new Ae2RecipeHandler<>());
+        registry.addRecipeHandler(WCTMenu.TYPE, new Ae2CraftingHandler<>(WCTMenu.class));
+        registry.addRecipeHandler(WETMenu.TYPE, new Ae2PatternTerminalHandler<>(WETMenu.class));
     }
 }
