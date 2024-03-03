9fe092f20ae9ff3e5d7a200a6fcb7e603a820081
alikindsys <alice@blocovermelho.org>
2024-03-03 17:00:31 -0300

    Add MainConfig.java

---
+++ src/main/java/org/blocovermelho/ae2emicrafting/client/config/MainConfig.java
@@ -0,0 +1,17 @@
+package org.blocovermelho.ae2emicrafting.client.config;
+
+import me.shedaniel.autoconfig.ConfigData;
+import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.autoconfig.annotation.ConfigEntry;
+
+@Config(name = "ae2-emi-crafting")
+public class MainConfig implements ConfigData {
+    @ConfigEntry.Category("emi")
+    @ConfigEntry.Gui.Tooltip()
+    public boolean bomsync = false;
+    @ConfigEntry.Category("integration")
+    @ConfigEntry.Gui.Tooltip()
+    @ConfigEntry.Gui.RequiresRestart
+    public boolean exmi = true;
+
+}
