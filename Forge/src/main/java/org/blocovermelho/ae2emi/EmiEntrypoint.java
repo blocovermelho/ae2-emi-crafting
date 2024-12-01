a756d141881d4228856f3f043ed1f00a77267878
alikindsys <alice@blocovermelho.org>
2024-12-01 14:55:44 -0300

    Initial Commit

---
+++ Forge/src/main/java/org/blocovermelho/ae2emi/EmiEntrypoint.java
@@ -0,0 +1,11 @@
+package org.blocovermelho.ae2emi;
+
+import dev.emi.emi.api.EmiPlugin;
+import dev.emi.emi.api.EmiRegistry;
+
+public class EmiEntrypoint implements EmiPlugin {
+    @Override
+    public void register(EmiRegistry emiRegistry) {
+
+    }
+}
---
+++ Forge/src/main/java/org/blocovermelho/ae2emi/ForgeEntrypoint.java
@@ -0,0 +1,8 @@
+package org.blocovermelho.ae2emi;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("ae2emi")
+public class ForgeEntrypoint {
+
+}
